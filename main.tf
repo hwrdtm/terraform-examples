5b41e869c25c07e7edc9878ee0eade60abc03f6b
Howard Tam <howard.tam.95@gmail.com>
2024-02-20 16:31:05 -0800

    Initial commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("cayenne-410322-c9a8c9f5a371.json")
+
+  project = "cayenne-410322"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
